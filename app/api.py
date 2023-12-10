b7952800dd98a3a66b3066fe1e6411f55bd02280
josealm22 <josealm2291@gmail.com>
2023-12-10 12:33:46 +0100

    MOD ERROR 406 EN DIVISION

--- app/api.py
+++ app/api.py
@@ -44,6 +44,11 @@ def multiply(op_1, op_2):
 def divide(op_1, op_2):
     try:
         num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+
+        # Comprobar si el divisor es cero
+        if num_2 == 0:
+            return ("División por cero no permitida", http.client.NOT_ACCEPTABLE, HEADERS)
+
         return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
